838710ae381f8e196e6d02b0a506aaadd882d7b7
smckinless <smckinless@gmail.com>
2015-11-18 23:46:11 -0500

    added port

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from flask import Flask, render_template, request
 from wtforms import Form, FloatField, validators
 from compute import compute
+import os
 
 app = Flask(__name__)
 
@@ -23,4 +24,5 @@ def index():
 
 
 if __name__ == '__main__':
-	app.run(host='0.0.0.0', port=5000)
+	port = int(os.environ.get("PORT", 5000))
+	app.run(host='0.0.0.0', port=port)
